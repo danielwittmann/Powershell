cb52e17778422950d5f07e63d95ee16977d00fc7
danielwittmann <daniel.wittmann@cellent.de>
2014-04-22 15:05:59 +0200

    commited

---
+++ Write-HelloWorld.ps1
@@ -0,0 +1,15 @@
+﻿<#
+-------------------------------------------------------------------------
+SCRIPT:                  Write-HelloWorld.ps1
+AUTHOR:                  Daniel Wittmann
+DATE:                    2014/04/24
+VERSION:                 1.0
+MIN. PS VERSION:         4.0
+SYNOPSIS:                Writes Hello World to console.
+-------------------------------------------------------------------------
+#>
+param (
+$Message = "Hello World"
+)
+
+Write-Output $Message
